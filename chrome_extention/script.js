1827d41b158d90dd8a05327245d016ad2be34680
Shashank Vats <63522097+shashnkvats@users.noreply.github.com>
2023-07-24 12:25:26 +0530

    Update script.js with latest redirect url for react app

--- chrome_extention/script.js
+++ chrome_extention/script.js
@@ -3,6 +3,6 @@ document.getElementById('getJobId').addEventListener('click', () => {
     let url = tabs[0].url;
     let jobId = url.substring(url.lastIndexOf('=') + 1);
     document.getElementById('jobIdDisplay').innerText = jobId;
-    chrome.tabs.create({ url: `http://localhost:3000/?jobId=${jobId}` });
+    chrome.tabs.create({ url: `https://resuminate-frontend-lozf7taqoa-uc.a.run.app/?jobID=${jobId}` });
   });
 });
